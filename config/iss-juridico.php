4130efc290610d452769d6a26bc7634d43f793cc
Noemi <noemimpimentel@gmail.com>
2023-12-21 09:23:21 -0300

    base_uri

--- config/iss-juridico.php
+++ config/iss-juridico.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'base_uri' => env('MS_JURIDICO_BASE_URI', 'https://server-juridico.modular.dev.br'),
-    'prefix' => env('MS_JURIDICO_API_PREFIX', 'api'),
+    'base_uri' => env('MS_JURIDICO_BASE_URI', 'https://contratos-server.nave.dev.br'),
+    'prefix' => env('MS_JURIDICO_API_PREFIX', 'api')
 ];
