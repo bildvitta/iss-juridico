9885858e9c015ec6d0ebe26dd0cd4db9fa6ebfe9
noemimpimentel <noemimpimentel@gmail.com>
2024-07-23 13:35:47 -0300

    created endpoint file

---
+++ src/Http/Controllers/Documents/FileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bildvitta\IssJuridico\Http\Controllers\Documents;
+
+use Bildvitta\IssJuridico\Http\Requests\Documents\ViewRequest;
+use Illuminate\Http\JsonResponse;
+
+class FileController extends DocumentController
+{
+    public function __invoke(ViewRequest $request): JsonResponse
+    {
+        $response = app('juridico')->documents()->file($request);
+        
+        return response()->json(
+            $response, $response->status->code
+        );
+    }
+}
